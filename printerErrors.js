20ee6d58f2c8acfb2691291c0502c075016d74ca
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-08-02 00:00:39 +0800

    modify solution but not yet complete

--- printerErrors.js
+++ printerErrors.js
@@ -1,9 +1,30 @@
-
+var sLength, error, typeOf, range, frac;
 
 function printerError(s) {
-    
-    
+//set a model
+    range = s.match(/[n-z\d\s\W]/ig);
+    sLength = s.length;//denominator
+
+//create a fraction
+		
+
+//Decision
+	if (range === null){
+		range = 0;
+		console.log(range + "/" + sLength);
+	} else if (range != null && error > 0 ){
+		error = range.length;//numerator
+		frac = error + "/" + sLength;
+		console.log(frac);
+	}
+	//console.log(range);
 }
+
+printerError('aaabbbbhaijjjm');
+printerError('aaaxbbbbyyhwawiwjjjwwm');
+
+
+
 
 
 
