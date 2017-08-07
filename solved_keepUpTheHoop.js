815caad0f5fa612836b389fc61779460148a0c88
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-08-07 23:23:18 +0800

    solve kata

---
+++ solved_keepUpTheHoop.js
@@ -0,0 +1,12 @@
+function hoopCount (n) {
+   //your code goes here
+   if (n >= 10){
+   		return 'Great, now move on to tricks';
+   }
+   else if (n <= 10 && n >= 0){
+   		return 'Keep at it until you get it';
+   }
+}
+
+hoopCount(3);
+hoopCount(11);
