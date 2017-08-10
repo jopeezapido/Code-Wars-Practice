73df571111374ef6645e6fb8372206677a97f662
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-08-10 10:00:32 +0800

    continue challenge

--- titleCase.js
+++ titleCase.js
@@ -3,17 +3,20 @@ var splitMinor;
 var newArray;
 var check;
 var searchy;
+var rep;
+var findWord;
 
 function titleCase(title, minorWords) {
 	splitTitle = title.split(/\s/ig);
-	//splitMinor = minorWords.split(/\s/ig);
-	check = splitTitle.map(function(element){
-		
-		console.log(searchy);
-	})
-	//console.log(splitTitle);
-}
+	splitMinor = minorWords.split(/\s/ig);
 
+	var findTitleWord = splitTitle.filter(function(element){
+		return element.match(//ig)
+	});
+
+	console.log(findTitleWord);
+}
+	
 titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
 titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
-titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+//titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
