3d5544e157bb6b03884fe1562fc11a5a24a57ffa
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-08-13 13:54:39 +0800

    modify file

--- titleCase.js
+++ titleCase.js
@@ -1,22 +1,9 @@
-var splitTitle;
-var splitMinor;
-var newArray;
-var check;
-var searchy;
-var rep;
-var findWord;
 
-function titleCase(title, minorWords) {
-	splitTitle = title.split(/\s/ig);
-	splitMinor = minorWords.split(/\s/ig);
-
-	var findTitleWord = splitTitle.filter(function(element){
-		return element.match(//ig)
-	});
-
-	console.log(findTitleWord);
-}
+function titleCase(title,minorWords){
 	
+	
+}
+
 titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
 titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
-//titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
