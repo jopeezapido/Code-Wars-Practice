52636563d89bb6c1d7d3ed2698c75e0b1c2a3486
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-22 11:59:13 +0800

    start solving problem

---
+++ isograms.js
@@ -0,0 +1,17 @@
+function isIsogram(str) {
+    //Determine if string contains only letters
+    //I need something that determines whether the argument passed (str) has no repeating letters
+    
+
+    //I need something to split the strings into chunks in an array
+
+    //I need a something to filter out the 
+    
+    //I need something to test if pattLet is and pattNoRep is met
+
+}
+
+
+isIsogram( "Dermatoglyphics" )//==true
+isIsogram( "aba" )//==false
+isIsogram( "moOse" )//== false, ignore letter case
