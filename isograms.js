73f82f7313ddfaa0ae82a6e542aabce1d6c47b1c
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-22 13:13:29 +0800

    create pseudo code

--- isograms.js
+++ isograms.js
@@ -1,13 +1,24 @@
 function isIsogram(str) {
-    //Determine if string contains only letters
-    //I need something that determines whether the argument passed (str) has no repeating letters
-    
+    //Splits a string and return an array
 
-    //I need something to split the strings into chunks in an array
+    //Create a pattern to base on
 
-    //I need a something to filter out the 
+    //Create an empty array
+
+    //Get the number of how many times an item in the array appeared in the string
+
+    //If an item in the array appeared one time only, push that item in empty array
+
+    //If an item in the array appeared MORE THAN ONCE, don't push that item to the new Array
+
+    //Transform the new Array into a string
+
+    //Now check the the newy joined string if it is equal to the STR argument
+
+    //If njs is equal to STR, return the boolean true
+
+    //If njs is not equal to STR, return the boolean false
     
-    //I need something to test if pattLet is and pattNoRep is met
 
 }
 
